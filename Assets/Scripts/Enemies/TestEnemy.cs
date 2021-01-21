9ef37b0fdc54d0f839858c0a15f6c7fdb3869746
TheSpiceyCatto <77725193+TheSpiceyCatto@users.noreply.github.com>
2021-01-21 22:16:27 +0100

    TestEnemy Script - bad Escape() edition
    +added code
    -fix Escape() asap

--- Assets/Scripts/Enemies/TestEnemy.cs
+++ Assets/Scripts/Enemies/TestEnemy.cs
@@ -5,41 +5,73 @@ using Lean.Pool;
 
 public class TestEnemy : MonoBehaviour
 {
-    public float reloadTime = 5f;
-    public float rotateTime = 5f;
+    [SerializeField] float moveForce = 25;
     public GameObject bulletPrefab;
     public Transform shootPoint;
+    public float rotateSeconds = 2f;
+    public float rotationSpeed = 10f;
     public float bulletSpeed;
     public float bulletLifetime = 2f;
     public float bulletRotationSpeed = 1f;
     public float bulletSeekDistance = 4f;
+    Rigidbody rb;
+    float randomAngle;
 
     string playerTag = "Player";
-    float cur_reloadTime;
+    private IEnumerator coroutine;
+    private bool reload = false;
 
-    private void Start()
+    void Start()
     {
-        cur_reloadTime = reloadTime;
+        rb = GetComponent<Rigidbody>();
+        Vector3 dir = Vector3.zero - transform.position;
+
+        //once spawned outside of the map go towards the center for x seconds - done
+        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.rotation = targetRotation;
+        coroutine = SearchAndDestroy(rotateSeconds);
+        StartCoroutine(coroutine);
+    }
+    IEnumerator SearchAndDestroy(float waitTime)
+    {
+        rb.AddForce(transform.forward * moveForce * 10f);
+        yield return new WaitForSeconds(waitTime);
+        while (true)
+        {
+            reload = true;
+            yield return new WaitForSeconds(waitTime);
+            reload = false;
+            Shoot();
+            Escape();
+            yield return new WaitForSeconds(waitTime * 2);
+        }
+
     }
     void Update()
     {
-        cur_reloadTime -= 0.01f;
-        if (cur_reloadTime < 0)
+        randomAngle = Random.Range(0.1f, 1f);
+        if (reload == true)
         {
-            SeekAndDestroy();
-            Shoot();
-            Escape();
+            //rotate towards player for 1-2sec - done
+            GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+            Vector3 dir = player.GetComponent<Transform>().position - transform.position;
+            float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            rb.AddForce(transform.forward * moveForce * 0.01f);
         }
     }
-    private void SeekAndDestroy()
-    {
-        cur_reloadTime = reloadTime;
-        //rotate towards player for 1-2sec
-    }
+
     private void Escape()
     {
-        //rotate in a random direction away from the player and escape
-        //dirToPlayer* Quaternion.AngleAxis(-randomAngle, Vector3.up)
+        //rotate in a random direction away from the player and escape - malfunction
+        GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+        Vector3 dir = player.GetComponent<Transform>().position - transform.position;
+        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.rotation = targetRotation * Quaternion.AngleAxis(-randomAngle, Vector3.up);
+        rb.AddForce(transform.forward * moveForce * 25f);
     }
     private void Shoot()
     {
