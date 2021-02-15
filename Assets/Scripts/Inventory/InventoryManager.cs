e9f2d4e6c9e931e33925fa815f60099439230687
qer24 <qer24@go2.pl>
2021-02-15 15:05:30 +0100

    Weapon swapping

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    [SerializeField] Weapon[] weapons = null;
+    [SerializeField] List<Weapon> weapons = new List<Weapon>();
     public Weapon CurrentWeapon { get => weapons[0];}
+
+    [SerializeField] LeanTweenType tweenType = LeanTweenType.linear;
+    [SerializeField] float tweenDuration = 0.2f;
+
+    float switchCooldownTimer;
+
+    private void Start()
+    {
+        switchCooldownTimer = tweenDuration;
+    }
+
+    public void Update()
+    {
+        if(switchCooldownTimer > 0)
+        {
+            switchCooldownTimer -= Time.deltaTime;
+        }
+        else if(Input.GetKeyDown(KeyCode.Tab) && weapons[1] != null)
+        {
+            switchCooldownTimer = tweenDuration * 4f;
+            SwapWeapon();
+        }
+    }
+
+    void SwapWeapon()
+    {
+        Vector3 firstWeaponPos = weapons[0].transform.parent.position;
+        Vector3 secondWeaponPos = weapons[1].transform.parent.position;
+
+        LeanTween.moveX(weapons[0].transform.parent.gameObject, secondWeaponPos.x, tweenDuration).setEase(tweenType);
+        LeanTween.moveX(weapons[1].transform.parent.gameObject, firstWeaponPos.x, tweenDuration).setEase(tweenType);
+
+        weapons.Reverse();
+    }
 }
