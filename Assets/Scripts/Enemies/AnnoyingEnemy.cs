db800aba750bcdd6b759be37edf957b9bfb9650e
TheSpiceyCatto <77725193+TheSpiceyCatto@users.noreply.github.com>
2021-02-23 20:11:22 +0100

    Annoying enemy added

---
+++ Assets/Scripts/Enemies/AnnoyingEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnnoyingEnemy : Enemy
+{
+    public float rotationSpeed = 10f;
+    public float moveForce = 25;
+    void Update()
+    {
+        Vector3 dirToPlayer = (player.position - transform.position).normalized;
+        float angle = Mathf.Atan2(dirToPlayer.x, dirToPlayer.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        rb.AddForce(transform.forward * moveForce * 0.02f);
+    }
+}
